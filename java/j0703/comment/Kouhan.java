21c44ed69942e98fd44e42bd712fad155f793d6b
room202 <84225189+room202@users.noreply.github.com>
2024-07-03 13:29:34 +0900

    Add files via upload

--- java/j0703/comment/Kouhan.java
+++ java/j0703/comment/Kouhan.java
@@ -7,6 +7,8 @@ public class Kouhan {
     public static void showMondokoro() throws Exception {
         System.out.println("飛車さん、角さん。もういいでしょう。");
         System.out.println("この紋所が目にはいらぬか！");
+        // 3秒の待ち時間を入れたい
+        java.lang.Thread.sleep(3000);
         Zenhan.doTogame();
     }
 }
